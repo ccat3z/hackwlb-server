9693ca28e475dce89e057a55512ab32795a439b5
c0ldcat <firez199984@gmail.com>
2016-11-13 11:19:23 +0800

    remove support of docker

--- owncloud.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Linking webapp owncloud..."
-
-ln -s /usr/share/webapps/owncloud /var/www/localhost/htdocs/
-
-echo "Setting php upload/post max size ad 512M..."
-
-sed -i "s/^\([^_]*\_max\_[^_]*size = \)[0-9]*M$/\1512M/g" /etc/php5/php.ini
